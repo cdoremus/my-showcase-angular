9ac6d16d631c34816480a6af74668fc2ed83664f
Craig Doremus <cdoremus@gmail.com>
2017-01-09 23:10:44 -0500

    Removed username property

--- src/app/shared/user.ts
+++ src/app/shared/user.ts
@@ -1,11 +1,10 @@
-interface User {
+export interface User {
   userId: number;
   loginId: string;
-  username: string;
   firstName: string;
   lastName: string;
   email: string;
   createDate: string;
 }
 
-export default User;
+
