71056e011c8de9f62e657b2c60dd802b184214b8
Craig Doremus <cdoremus@gmail.com>
2017-01-09 23:14:23 -0500

    Added URLs to uploading file and saving metadata

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,5 +1,7 @@
 
 const BASE_URL = 'http://localhost:8080';
 export const config = {
-  loginUrl: BASE_URL + '/user/findByLoginId?loginId='
+  loginUrl: BASE_URL + '/user/findByLoginId?loginId=',
+  fileUploadUrl: BASE_URL + '/displayItem/upload',
+  saveFileMetadata: BASE_URL + '/displayItem/save'
 };
